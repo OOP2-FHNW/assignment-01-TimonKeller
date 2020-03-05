6b13b7831069b8413bcb92e6193a62837c581cda
Keller Timon (s) <timon.keller@students.fhnw.ch>
2020-03-05 18:45:09 +0100

    Assignment

--- src/main/java/portfolio/Portfolio.java
+++ src/main/java/portfolio/Portfolio.java
@@ -1,5 +1,64 @@
 package portfolio;
 
-public class Portfolio {
-    //TODO Complete this implementation and make it generic
+
+import portfolio.investments.Investment;
+
+import java.util.HashSet;
+
+public class Portfolio<T extends Investment> {
+
+    HashSet<T> investments = new HashSet<>();
+
+    public Portfolio() {
+    }
+
+    public boolean contains(T ubs) {
+
+        for (T shares : investments) {
+            if (shares.equals(ubs)) {
+                return true;
+            }
+
+        } return false;
+    }
+
+    public void buy(T ubs) {
+
+        if (investments.contains(ubs)) {
+            for (T shares : investments) {
+                if (!shares.equals(ubs)) {
+                    investments.add(ubs);
+                }
+            }
+        } else {
+            for(T shares : investments)
+            shares.setCount(shares.getCount() + 1);
+        }
+    }
+
+    public void sell(String ubs, double number) {
+
+        T shares = getShare(ubs);
+        if (shares != null) {
+            shares.setCount(shares.getCount() - number);
+            if (shares.getCount() == 0) {
+                shares.setCount(0);
+                investments.remove(shares);
+            }
+        }
+    }
+
+
+    public T getShare(String ubs) {
+
+        for (T shares : investments) {
+            if (shares.getTitel().equals(ubs)) {
+                return shares;
+            } else {
+                return null;
+            }
+        }
+        return null;
+    }
 }
+
